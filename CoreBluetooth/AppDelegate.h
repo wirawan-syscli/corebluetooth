efcbfc56f6f03cbcd869ce9f43e76607f064eb18
wirawan-syscli <wirawan@syscli.co.jp>
2015-05-29 12:18:36 +0900

    Initial Commit

---
+++ CoreBluetooth/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoreBluetooth
+//
+//  Created by wirawan sanusi on 5/29/15.
+//  Copyright (c) 2015 wirawan sanusi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoreBluetooth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoreBluetooth
+//
+//  Created by wirawan sanusi on 5/29/15.
+//  Copyright (c) 2015 wirawan sanusi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
